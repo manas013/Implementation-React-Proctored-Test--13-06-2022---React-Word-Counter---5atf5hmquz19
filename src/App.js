be545ed69702d557d554afe7af20206cc400bcc6
manas013 <mukherjee18m@gmail.com>
2022-06-13 22:02:18 +0530

    da

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Kj from './Kj';
+
+function App() {
+  return (
+    <div className="App">
+      <Kj/>
+    </div>
+  );
+}
+
+export default App;
